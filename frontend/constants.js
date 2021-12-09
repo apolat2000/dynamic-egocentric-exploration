1eb6ceda8ad45e6acf7317feb12d69ecf48b39d8
Ahmet Polat <polat2000@gmail.com>
2021-12-09 18:36:29 +0100

    visual improvs for egocentric

--- frontend/constants.js
+++ frontend/constants.js
@@ -1,23 +1,23 @@
-const explorationType = "free";
+const explorationType = "egoHighlight";
 // const explorationType = "free";
-// const explorationType = "egoBubble";
-// const explorationType = "egoHighlight";
 
 const isProduction = false;
 
-const defaultURL = "https://apolat2000.github.io/";
+// const defaultURL = "https://apolat2000.github.io/";
+// const defaultURL = "https://moodle.rwth-aachen.de/";
+const defaultURL = "https://www.instagram.com/astarwth/?hl=en";
 
 const nodeSize = 4;
 const nodeResolution = nodeSize * 2;
 
-const linkWidth = 0.8;
+const linkWidth = explorationType === "free" ? 0.8 : 0.5;
 const linkColor = 0xffffff;
 const linkOpacity = 0.2;
 
-const arrowLength = 6;
+const arrowLength = explorationType === "free" ? 6 : 0;
 
 const frameGraphAttributeAppend = `num-dimensions: 3; link-curvature: ${
-  explorationType === "free" ? "0.05" : "0"
+  explorationType === "free" ? "0.05" : "-0.5"
 }; link-directional-arrow-length: ${arrowLength}; link-color: ${linkColor};
 link-opacity: ${linkOpacity}; node-auto-color-by: isBeingHovered; link-width: ${linkWidth};
 link-directional-arrow-rel-pos: 1; node-rel-size: ${nodeSize}; node-three-object: node => nodeObjectHandler(node);
